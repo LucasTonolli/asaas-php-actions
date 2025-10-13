3b66ca22eb1be0e7fbb2db94d9a7371d87d353da
Lucas Tonolli <lucastonolli973@gmail.com>
2025-10-13 11:45:41 -0300

    test: add value object phone test

---
+++ tests/Unit/ValueObjects/SimpleValueObjects/PhoneTest.php
@@ -0,0 +1,24 @@
+<?php
+
+use AsaasPhpSdk\Exceptions\InvalidPhoneException;
+use AsaasPhpSdk\ValueObjects\Phone;
+
+describe('Phone Value Object', function (): void {
+	it('can be created with a valid phone number', function (): void {
+		$phone = Phone::from('11999999999');
+		expect($phone->value())->toBe('11999999999');
+		expect($phone->formatted())->toBe('(11) 99999-9999');
+	});
+
+	it('cannot be created with an invalid phone number', function (): void {
+		expect(fn() => Phone::from('123456789'))->toThrow(InvalidPhoneException::class, 'Phone must contain 10 or 11 digits');
+	});
+
+	it('compare phone', function (): void {
+		$phone1 = Phone::from('11999999999');
+		$phone2 = Phone::from('11999999999');
+		$phone3 = Phone::from('11999999998');
+		expect($phone1->equals($phone2))->toBeTrue();
+		expect($phone1->equals($phone3))->toBeFalse();
+	});
+});
