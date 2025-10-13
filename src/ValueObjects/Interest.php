1b4883b512c660a133a6ecc0b3fbea54d9f33c71
Lucas Tonolli <lucastonolli973@gmail.com>
2025-10-13 12:56:19 -0300

    feat: add new exception

--- src/ValueObjects/Interest.php
+++ src/ValueObjects/Interest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace AsaasPhpSdk\ValueObjects;
 
+use AsaasPhpSdk\Exceptions\InvalidInterestException;
 use AsaasPhpSdk\ValueObjects\Base\AbstractStructuredValueObject;
 
 final class Interest extends AbstractStructuredValueObject
@@ -15,11 +16,11 @@ final class Interest extends AbstractStructuredValueObject
 	public static function create(float $value): self
 	{
 		if ($value < 0) {
-			throw new \InvalidArgumentException('Interest value cannot be negative');
+			throw new InvalidInterestException('Interest value cannot be negative');
 		}
 
 		if ($value > 100) {
-			throw new \InvalidArgumentException('Interest value cannot exceed 100%');
+			throw new InvalidInterestException('Interest value cannot exceed 100%');
 		}
 
 		return new self($value);
@@ -28,7 +29,7 @@ final class Interest extends AbstractStructuredValueObject
 	public static function fromArray(array $data): self
 	{
 		return self::create(
-			value: $data['value'] ?? throw new \InvalidArgumentException('Interest value is required')
+			value: $data['value'] ?? throw new InvalidInterestException('Interest value is required')
 		);
 	}
 }
