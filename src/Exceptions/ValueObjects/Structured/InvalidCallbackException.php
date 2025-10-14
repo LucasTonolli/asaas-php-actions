17c9f4a37752bc52a44712201ad24db240f4e84e
Lucas Tonolli <lucastonolli973@gmail.com>
2025-10-14 10:47:14 -0300

    docs: add phpdocs

--- src/Exceptions/ValueObjects/Structured/InvalidCallbackException.php
+++ src/Exceptions/ValueObjects/Structured/InvalidCallbackException.php
@@ -6,4 +6,11 @@ namespace AsaasPhpSdk\Exceptions\ValueObjects\Structured;
 
 use AsaasPhpSdk\Exceptions\ValueObjects\InvalidValueObjectException;
 
+/**
+ * Represents an error for an invalid Callback configuration.
+ *
+ * This exception is typically thrown by the `Callback` Value Object when
+ * the provided data is invalid, such as a malformed URL or incorrect
+ * notification settings.
+ */
 class InvalidCallbackException extends InvalidValueObjectException {}
