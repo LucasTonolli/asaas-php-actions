f4eb29e32761b1453d63ad6f04323980e1e52f82
Lucas Tonolli <lucastonolli973@gmail.com>
2025-10-14 10:49:34 -0300

    docs: add phpdocs

--- src/Exceptions/ValueObjects/Structured/InvalidSplitException.php
+++ src/Exceptions/ValueObjects/Structured/InvalidSplitException.php
@@ -6,4 +6,12 @@ namespace AsaasPhpSdk\Exceptions\ValueObjects\Structured;
 
 use AsaasPhpSdk\Exceptions\ValueObjects\InvalidValueObjectException;
 
+/**
+ * Represents an error for an invalid payment Split configuration.
+ *
+ * This exception is typically thrown by the `Split` Value Object, which manages
+ * the entire collection of payment recipients. An error can occur if the split
+ * rules are violated, such as the sum of the split amounts not matching the
+ * total transaction value.
+ */
 class InvalidSplitException extends InvalidValueObjectException {}
