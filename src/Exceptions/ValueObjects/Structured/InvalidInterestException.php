80beb9161f49499c378721757ac36da1b5baa450
Lucas Tonolli <lucastonolli973@gmail.com>
2025-10-14 10:48:37 -0300

    docs: add phpdocs

--- src/Exceptions/ValueObjects/Structured/InvalidInterestException.php
+++ src/Exceptions/ValueObjects/Structured/InvalidInterestException.php
@@ -6,4 +6,11 @@ namespace AsaasPhpSdk\Exceptions\ValueObjects\Structured;
 
 use AsaasPhpSdk\Exceptions\ValueObjects\InvalidValueObjectException;
 
+/**
+ * Represents an error for an invalid Interest (juros) configuration.
+ *
+ * This exception is typically thrown by the `Interest` Value Object when the
+ * provided data is invalid, such as a negative value or an unknown
+ * interest type.
+ */
 class InvalidInterestException extends InvalidValueObjectException {}
