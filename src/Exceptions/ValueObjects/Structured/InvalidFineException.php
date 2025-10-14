c5751eaf0e089ec9b59ff4656cf1b502e271b8a8
Lucas Tonolli <lucastonolli973@gmail.com>
2025-10-14 10:48:11 -0300

    docs: add phpdocs

--- src/Exceptions/ValueObjects/Structured/InvalidFineException.php
+++ src/Exceptions/ValueObjects/Structured/InvalidFineException.php
@@ -6,4 +6,11 @@ namespace AsaasPhpSdk\Exceptions\ValueObjects\Structured;
 
 use AsaasPhpSdk\Exceptions\ValueObjects\InvalidValueObjectException;
 
+/**
+ * Represents an error for an invalid Fine (multa) configuration.
+ *
+ * This exception is typically thrown by the `Fine` Value Object when the
+ * provided data is invalid, such as a negative value or an unknown
+ * fine type.
+ */
 class InvalidFineException extends InvalidValueObjectException {}
