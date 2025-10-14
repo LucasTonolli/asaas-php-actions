1e60211d49cd3ff90d3ed6e46e5844a1c23023da
Lucas Tonolli <lucastonolli973@gmail.com>
2025-10-14 10:49:04 -0300

    docs: add phpdocs

--- src/Exceptions/ValueObjects/Structured/InvalidSplitEntryException.php
+++ src/Exceptions/ValueObjects/Structured/InvalidSplitEntryException.php
@@ -6,4 +6,11 @@ namespace AsaasPhpSdk\Exceptions\ValueObjects\Structured;
 
 use AsaasPhpSdk\Exceptions\ValueObjects\InvalidValueObjectException;
 
+/**
+ * Represents an error for an invalid Split Entry configuration.
+ *
+ * This exception is typically thrown by the `SplitEntry` Value Object, which
+ * represents a single recipient in a payment split. The error can occur if the
+ * data is invalid, such as a missing recipient wallet ID or an invalid amount.
+ */
 class InvalidSplitEntryException extends InvalidValueObjectException {}
