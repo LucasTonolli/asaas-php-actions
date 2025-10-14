fd914a60d50ef155b7b1ab32a20149a69c8fd99d
Lucas Tonolli <lucastonolli973@gmail.com>
2025-10-14 11:35:59 -0300

    docs: add phpdocs

--- src/Actions/Payments/CreatePaymentAction.php
+++ src/Actions/Payments/CreatePaymentAction.php
@@ -9,6 +9,23 @@ use AsaasPhpSdk\DTOs\Payments\CreatePaymentDTO;
 
 final class CreatePaymentAction extends AbstractAction
 {
+    /**
+     * Creates a new payment.
+     *
+     * This action sends a POST request to the 'payments' endpoint. The data is
+     * encapsulated and validated by the CreatePaymentDTO before being sent.
+     *
+     * @see https://docs.asaas.com/reference/criar-nova-cobranca Official Asaas API Documentation
+     *
+     * @param  CreatePaymentDTO  $data A Data Transfer Object containing the validated payment data.
+     * @return array An array representing the newly created payment as returned by the API.
+     *
+     * @throws AuthenticationException
+     * @throws NotFoundException
+     * @throws ValidationException
+     * @throws RateLimitException
+     * @throws ApiException
+     */
     public function handle(CreatePaymentDTO $data): array
     {
         return $this->executeRequest(
