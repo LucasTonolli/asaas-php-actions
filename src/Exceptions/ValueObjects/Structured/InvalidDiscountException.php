887211d426dd8ae24c94116d37c5b13674ed439c
Lucas Tonolli <lucastonolli973@gmail.com>
2025-10-14 10:47:42 -0300

    docs: add phpdocs

--- src/Exceptions/ValueObjects/Structured/InvalidDiscountException.php
+++ src/Exceptions/ValueObjects/Structured/InvalidDiscountException.php
@@ -6,4 +6,11 @@ namespace AsaasPhpSdk\Exceptions\ValueObjects\Structured;
 
 use AsaasPhpSdk\Exceptions\ValueObjects\InvalidValueObjectException;
 
+/**
+ * Represents an error for an invalid Discount configuration.
+ *
+ * This exception is typically thrown by the `Discount` Value Object when the
+ * provided data is invalid, such as a negative value or an unknown
+ * discount type.
+ */
 class InvalidDiscountException extends InvalidValueObjectException {}
