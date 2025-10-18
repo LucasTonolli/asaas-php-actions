4fea54f79cf8f2f1cd44cbd609df80c25b3cde45
Lucas Tonolli <lucastonolli973@gmail.com>
2025-10-18 15:06:07 -0300

    test: add Restore Customer Integration

---
+++ tests/Integration/Http/Payments/RestorePaymentTest.php
@@ -0,0 +1,44 @@
+<?php
+
+use AsaasPhpSdk\DTOs\Payments\Enums\BillingTypeEnum;
+use AsaasPhpSdk\Exceptions\Api\NotFoundException;
+
+describe('Restore Payment', function () {
+	beforeEach(function () {
+		$config = sandboxConfig();
+		$this->asaasClient = new AsaasPhpSdk\AsaasClient($config);
+	});
+
+	it('restores a payment successfully', function () {
+		$customerId = getDefaultCustomer();
+
+		$createPaymentResponse = $this->asaasClient->payment()->create([
+			'customer' => $customerId,
+			'value' => random_int(100, 1000),
+			'dueDate' => date('Y-m-d'),
+			'billingType' => BillingTypeEnum::Pix->value,
+		]);
+
+		$this->asaasClient->payment()->delete($createPaymentResponse['id']);
+
+		$response = $this->asaasClient->payment()->restore($createPaymentResponse['id']);
+		expect($response['id'])->toBe($createPaymentResponse['id'])
+			->and($response['deleted'])->toBe(false)
+			->and($response)->toHaveKeys([
+				'id',
+				'customer',
+				'value',
+				'dueDate',
+				'billingType',
+				'status',
+			]);
+	});
+
+	it('throws an exception when the payment is not found (404)', function () {
+		$this->asaasClient->payment()->restore('pay_notfound');
+	})->throws(NotFoundException::class, 'Resource not found');
+
+	it('throws an exception when the payment ID is empty', function () {
+		$this->asaasClient->payment()->restore('');
+	})->throws(\InvalidArgumentException::class, 'Payment ID cannot be empty');
+});
