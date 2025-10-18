b47d35065abc980b9a11cf947fbc1b86e9526263
Lucas Tonolli <lucastonolli973@gmail.com>
2025-10-18 16:50:54 -0300

    build: add custom boostrap for tests

---
+++ tests/bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+
+$asaasClient = new AsaasPhpSdk\AsaasClient(sandboxConfig());
+$payments = $asaasClient->payment()->list([
+	'limit' => 100,
+	'status' => 'PENDING',
+]);
+
+foreach ($payments['data'] as $payment) {
+	$asaasClient->payment()->delete($payment['id']);
+}
