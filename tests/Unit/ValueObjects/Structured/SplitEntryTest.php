797f54363608941b002d767f9f5c7b1c268f93f0
Lucas Tonolli <lucastonolli973@gmail.com>
2025-10-24 16:13:35 -0300

    test: refactor

--- tests/Unit/ValueObjects/Structured/SplitEntryTest.php
+++ tests/Unit/ValueObjects/Structured/SplitEntryTest.php
@@ -15,11 +15,12 @@ describe('Split Entry Value Object', function (): void {
             ->and($splitEntry->externalReference)->toBeNull()
             ->and($splitEntry->description)->toBeNull();
 
-        $splitEntry = SplitEntry::create(
-            walletId: 'wallet_id',
-            percentageValue: 25.0,
-            description: 'Test description'
-        );
+        $splitEntry = SplitEntry::fromArray([
+            'walletId' => 'wallet_id',
+            'percentageValue' => 25.0,
+            'description' => 'Test description',
+        ]);
+
         expect($splitEntry->percentageValue)->toBe(25.0);
         expect($splitEntry->description)->toBe('Test description');
         expect($splitEntry->totalFixedValue)->toBeNull()->and($splitEntry->externalReference)->toBeNull()
@@ -27,37 +28,34 @@ describe('Split Entry Value Object', function (): void {
     });
 
     it('cannot be created with an invalid split entry', function (): void {
-        expect(fn () => SplitEntry::fromArray([
+        expect(fn() => SplitEntry::fromArray([
             'walletId' => 'wallet_id',
         ]))->toThrow(InvalidSplitEntryException::class, 'At least one value must be provided');
 
-        expect(fn () => SplitEntry::create(
-            walletId: 'wallet_id',
-            percentageValue: 101.0,
-            description: 'Test description'
-        ))->toThrow(InvalidSplitEntryException::class, 'Percentage value must be between 0 and 100');
+        expect(fn() => SplitEntry::fromArray([
+            'walletId' => 'wallet_id',
+            'percentageValue' => 101,
+            'description' => 'Test description',
+        ]))->toThrow(InvalidSplitEntryException::class, 'Percentage value must be between 0 and 100');
     });
 
     it('walletId is required', function (): void {
-        expect(fn () => SplitEntry::fromArray([]))->toThrow(InvalidSplitEntryException::class, 'walletId is required');
+        expect(fn() => SplitEntry::fromArray([]))->toThrow(InvalidSplitEntryException::class, 'walletId is required');
     });
 
     it('compares the same split entry', function (): void {
-        $splitEntry1 = SplitEntry::create(
-            walletId: 'wallet_id',
-            percentageValue: 25.0,
-            description: 'Test description'
-        );
-        $splitEntry2 = SplitEntry::create(
-            walletId: 'wallet_id',
-            percentageValue: 25.0,
-            description: 'Test description'
-        );
-        $splitEntry3 = SplitEntry::create(
-            walletId: 'wallet_id2',
-            percentageValue: 25.0,
-            description: 'Test description'
-        );
+        $splitEntry1 = SplitEntry::fromArray([
+            'walletId' => 'wallet_id',
+            'fixedValue' => 10.0,
+        ]);
+        $splitEntry2 = SplitEntry::fromArray([
+            'walletId' => 'wallet_id',
+            'fixedValue' => 10.0,
+        ]);
+        $splitEntry3 = SplitEntry::fromArray([
+            'walletId' => 'wallet_id',
+            'percentageValue' => 20.0,
+        ]);
         expect($splitEntry1->equals($splitEntry2))->toBeTrue();
         expect($splitEntry1->equals($splitEntry3))->toBeFalse();
     });
