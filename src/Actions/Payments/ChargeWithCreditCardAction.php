02a30beffffbd7a51bd798cc1f0eda8c4f4875b8
Lucas Tonolli <lucastonolli973@gmail.com>
2025-10-24 16:56:33 -0300

    docs: add docs

--- src/Actions/Payments/ChargeWithCreditCardAction.php
+++ src/Actions/Payments/ChargeWithCreditCardAction.php
@@ -9,6 +9,25 @@ use AsaasPhpSdk\DTOs\Payments\ChargeWithCreditCardDTO;
 
 final class ChargeWithCreditCardAction extends AbstractAction
 {
+	/**
+	 * Charges a payment with a credit card by its ID.
+	 * 
+	 * This action performs a pre-request validation to ensure the ID is not
+	 * empty and then sends a POST request to the 'payments/{id}/payWithCreditCard' endpoint.
+	 *	
+	 * @see https://docs.asaas.com/reference/pagar-uma-cobranca-com-cartao-de-credito Official Asaas API Documentation
+	 * 
+	 * @param  string  $paymentId  The unique identifier of the payment to be charged.
+	 * @param  ChargeWithCreditCardDTO  $dto  The data transfer object containing the payment details.
+	 * @return array<string, mixed> An array containing the data of the charged payment.
+	 * 
+	 * @throws \InvalidArgumentException if the provided payment ID is empty.
+	 * @throws AuthenticationException
+	 * @throws NotFoundException
+	 * @throws ValidationException
+	 * @throws RateLimitException
+	 * @throws ApiException
+	 */
 	public function handle(string $paymentId, ChargeWithCreditCardDTO $dto): array
 	{
 		$normalizedId = trim($paymentId);
