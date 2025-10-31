d59a2a5ca1969733f8224c008b9b7db4c7593257
Lucas Tonolli <lucastonolli973@gmail.com>
2025-10-30 21:01:38 -0300

    refactor: add new abstraction in `GetCustomerAction`

--- src/Actions/Customers/GetCustomerAction.php
+++ src/Actions/Customers/GetCustomerAction.php
@@ -4,42 +4,27 @@ declare(strict_types=1);
 
 namespace AsaasPhpSdk\Actions\Customers;
 
-use AsaasPhpSdk\Actions\Base\AbstractAction;
-use AsaasPhpSdk\Exceptions\Api\ApiException;
-use AsaasPhpSdk\Exceptions\Api\AuthenticationException;
-use AsaasPhpSdk\Exceptions\Api\NotFoundException;
-use AsaasPhpSdk\Exceptions\Api\RateLimitException;
-use AsaasPhpSdk\Exceptions\Api\ValidationException;
+use AsaasPhpSdk\Actions\Base\GetByIdAction;
 
-final class GetCustomerAction extends AbstractAction
+/**
+ * Retrieves a single customer by their ID.
+ * 
+ * @see https://docs.asaas.com/reference/recuperar-um-unico-cliente Official Asaas API Documentation
+ */
+final class GetCustomerAction extends GetByIdAction
 {
     /**
-     * Retrieves a single customer by their ID.
-     *
-     * This action performs a pre-request validation to ensure the ID is not
-     * empty and then sends a GET request to the 'customers/{id}' endpoint.
-     *
-     * @see https://docs.asaas.com/reference/recuperar-um-unico-cliente Official Asaas API Documentation
-     *
-     * @param  string  $customerId  The unique identifier of the customer to be retrieved.
-     * @return array<string, mixed> An array containing the data of the specified customer.
-     *
-     * @throws \InvalidArgumentException if the provided customer ID is empty.
-     * @throws AuthenticationException
-     * @throws NotFoundException
-     * @throws ValidationException
-     * @throws RateLimitException
-     * @throws ApiException
+     * {@inheritDoc} 
      */
-    public function handle(string $customerId): array
+    protected function getResourceName(): string
     {
-        $normalizedId = trim($customerId);
-        if ($normalizedId === '') {
-            throw new \InvalidArgumentException('Customer ID cannot be empty');
-        }
-
-        return $this->executeRequest(
-            fn () => $this->client->get('customers/'.rawurlencode($normalizedId))
-        );
+        return 'Customer';
+    }
+    /**
+     * {@inheritDoc} 
+     */
+    protected function getEndpoint(string $id): string
+    {
+        return "customers/" . rawurlencode($id);
     }
 }
