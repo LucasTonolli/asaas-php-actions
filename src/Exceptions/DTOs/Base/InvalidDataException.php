3aeefb9b94cd4fd2544ba64e258ddcbbb4cc36ab
Lucas Tonolli <lucastonolli973@gmail.com>
2025-10-31 16:03:04 -0300

    feat: add new base exception for DTOs

---
+++ src/Exceptions/DTOs/Base/InvalidDataException.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AsaasPhpSdk\Exceptions\DTOs\Base;
+
+use AsaasPhpSdk\Exceptions\AsaasException;
+
+/**
+ * Base class for exceptions related to invalid data in DTOs.
+ */
+class InvalidDataException extends AsaasException {}
