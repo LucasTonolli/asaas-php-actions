6d89a96b25e9c0e453fd9c8f335b8048b1ca383a
Lucas Tonolli <lucastonolli973@gmail.com>
2025-11-03 10:59:55 -0300

    refactor: add default function to `InvalidDataException`

--- src/Exceptions/DTOs/Base/InvalidDataException.php
+++ src/Exceptions/DTOs/Base/InvalidDataException.php
@@ -9,4 +9,30 @@ use AsaasPhpSdk\Exceptions\AsaasException;
 /**
  * Base class for exceptions related to invalid data in DTOs.
  */
-class InvalidDataException extends AsaasException {}
+class InvalidDataException extends AsaasException
+{
+	/**
+	 * Creates an exception for a missing required field.
+	 *
+	 * @param  string  $field  The name of the required field that is missing.
+	 * @return self A new instance of the exception.
+	 */
+	public static function missingField(string $field): self
+	{
+		return new self("Required field '{$field}' is missing.", 400);
+	}
+
+	/**
+	 * Creates an exception for a field with an invalid format.
+	 *
+	 * @param  string  $field  The name of the field with the invalid format.
+	 * @param  ?string  $message  An optional, more specific error message. A default is used if not provided.
+	 * @return self A new instance of the exception.
+	 */
+	public static function invalidFormat(string $field, ?string $message = null): self
+	{
+		$defaultMessage = "Field '{$field}' has invalid format.";
+
+		return new self($message ?? $defaultMessage, 400);
+	}
+}
--- src/Exceptions/DTOs/CreditCard/InvalidCreditCardDataException.php
+++ src/Exceptions/DTOs/CreditCard/InvalidCreditCardDataException.php
@@ -14,30 +14,4 @@ use AsaasPhpSdk\Exceptions\DTOs\Base\InvalidDataException;
  * such as a missing required field or a malformed value. It uses static
  * factory methods for creating consistent error messages.
  */
-class InvalidCreditCardDataException extends InvalidDataException
-{
-    /**
-     * Creates an exception for a missing required field.
-     *
-     * @param  string  $field  The name of the required field that is missing.
-     * @return self A new instance of the exception.
-     */
-    public static function missingField(string $field): self
-    {
-        return new self("Required field '{$field}' is missing.", 400);
-    }
-
-    /**
-     * Creates an exception for a field with an invalid format.
-     *
-     * @param  string  $field  The name of the field with the invalid format.
-     * @param  ?string  $message  An optional, more specific error message. A default is used if not provided.
-     * @return self A new instance of the exception.
-     */
-    public static function invalidFormat(string $field, ?string $message = null): self
-    {
-        $defaultMessage = "Field '{$field}' has invalid format.";
-
-        return new self($message ?? $defaultMessage, 400);
-    }
-}
+class InvalidCreditCardDataException extends InvalidDataException {}
