cc2fd52cb20de3297d8016772993b8f794aebacc
Lucas Tonolli <lucastonolli973@gmail.com>
2025-11-26 16:21:48 -0300

    feat: add new action `GetWebhookAction``

---
+++ src/Actions/Webhooks/GetWebhookAction.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AsaasPhpSdk\Actions\Webhooks;
+
+use AsaasPhpSdk\Actions\Base\GetByIdAction;
+
+/**
+ * Retrieves a single webhook by its ID.
+ * 
+ * @see https://docs.asaas.com/reference/recuperar-um-unico-webhook Official Asaas API Documentation
+ */
+final class GetWebhookAction extends GetByIdAction
+{
+	/**
+	 * {@inheritDoc}
+	 */
+	protected function getResourceName(): string
+	{
+		return 'Webhook';
+	}
+	/**
+	 * {@inheritDoc}
+	 */
+	protected function getEndpoint(string $id): string
+	{
+		return 'webhooks/' . rawurlencode($id);
+	}
+}
